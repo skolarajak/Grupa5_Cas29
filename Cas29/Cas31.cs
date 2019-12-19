0cc830f9a0ce3629846b49583d709104f012d300
Skola Rajak QA <skolarajak.qa@gmail.com>
2019-12-19 22:14:20 +0100

    Updated tests, explored some NUnit annotations, included Selenium Actions in tests, covered window handling

--- Cas29/Cas31.cs
+++ Cas29/Cas31.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
 namespace Cas29
 {
@@ -294,10 +296,143 @@ namespace Cas29
             this.FindElement(By.XPath("//img[@class='lazyloading']"))?.Click();
         }
 
+        [Test, Category("Cas32")]
+        public void TestRightClickContextMenu()
+        {
+            this.NavigateTo("https://www.cssscript.com/demo/lightweight-context-menu-library-class2context-js/");
+            var action = new Actions(this.Driver);
+            IWebElement div = this.FindElement(By.XPath("//div[contains(@class, 'class1')]"));
+            action.ContextClick(div).Perform();
+            DoWait(1);
+            IWebElement a1 = this.FindElement(By.XPath("//a[contains(., 'A1')]"));
+            a1.Click();
+            DoWait(2);
+            try
+            {
+                var alert = this.Driver.SwitchTo().Alert();
+                alert.Accept();
+            }
+            catch(NoAlertPresentException e)
+            {
+                // Do something
+            }
+            DoWait(2);
+        }
+
+        [Test, Category("Cas32")]
+        public void TestScrollTo()
+        {
+            // Works in Chrome, doesn't work in Firefox
+            this.NavigateTo("https://www.toolsqa.com/automation-practice-form/");
+            DoWait(2);
+            IWebElement continents = this.FindElement(By.Id("continents"));
+            var action = new Actions(this.Driver);
+            action.MoveToElement(continents).Perform();
+            DoWait(2);
+        }
+
+        [Test]
+        [Category("Cas32")]
+        [MaxTime(60000)]
+        public void TestDragAndDrop()
+        {
+            // Doesn't work
+            this.NavigateTo("https://formy-project.herokuapp.com/dragdrop");
+            IWebElement target = this.FindElement(By.Id("box"));
+            IWebElement drop1 = this.FindElement(By.Id("image"));
+            var action = new Actions(this.Driver);
+            action.DragAndDrop(drop1, target).Perform();
+            /*
+            action.ClickAndHold(drop1);
+            action.MoveToElement(target);
+            action.Release();
+            action.Build();
+            action.Perform();
+            */
+            DoWait(2);
+        }
+
+        [Test, Category("Cas32")]
+        public void TestWindows()
+        {
+            this.NavigateTo("https://www.seleniumeasy.com/test/window-popup-modal-demo.html");
+            IWebElement twitter = this.FindElement(By.XPath("//a[contains(@class, 'followeasy') and contains(., 'Twitter')]"));
+            twitter.Click();
+            DoWait(1);
+            var popup = this.Driver.WindowHandles[1];
+            this.Driver.SwitchTo().Window(popup);
+            // Executes in popup window
+            DoWait(1);
+            IWebElement username = this.FindElement(By.Id("username_or_email"));
+            username.SendKeys("nekimail@negde.com");
+            DoWait(1);
+            IWebElement password = this.FindElement(By.Id("password"));
+            password.SendKeys("nekasifra");
+            DoWait(3);
+            this.Driver.Close(); // Closes popup window
+
+            DoWait(2);
+            this.Driver.SwitchTo().Window(this.Driver.WindowHandles[0]);
+        }
+
+        [Test, Category("Cas32")]
+        public void TestWaitForElement()
+        {
+            // Doesn't work
+            this.NavigateTo("http://test.qa.rs");
+            var findBy = By.XPath("//a[@href = '/new']");
+            var wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(20));
+            wait.Until(drv => drv.FindElement(findBy));
+            IWebElement link = this.FindElement(findBy);
+            link.Click();
+            DoWait(2);
+        }
+
+        [Test]
+        [Category("Cas32")]
+        public void Cas32_Aleksandar()
+        {
+            this.NavigateTo("https://www.seleniumeasy.com/test/window-popup-modal-demo.html");
+            IWebElement button1 = this.FindElement(By.XPath("//div[@class='two-windows']/a"));
+            //IWebElement button1 = this.FindElement(By.XPath("//a[contains(text(),'Follow Twitter & Facebook')]"));
+            button1.Click();
+            DoWait(1);
+            var mainWindow = this.Driver.CurrentWindowHandle;
+
+            foreach(var windowHandle in this.Driver.WindowHandles)
+            {
+                this.Driver.SwitchTo().Window(windowHandle);
+                string title = this.Driver.Title;
+                if (title.Contains("Twitter"))
+                {
+                    IWebElement username1 = this.FindElement(By.Id("username_or_email"));
+                    username1.SendKeys("mail1@gmail.com");
+                    DoWait(1);
+                    IWebElement password1 = this.FindElement(By.XPath("//input[@id='password']"));
+                    password1.SendKeys("sifra1");
+                    DoWait(1);
+                    this.Driver.Close();
+                }
+                if (title.Contains("Facebook"))
+                {
+                    IWebElement username2 = this.FindElement(By.CssSelector("#email"));
+                    username2.SendKeys("mail2@gmail.com");
+                    DoWait(1);
+                    IWebElement password2 = this.FindElement(By.CssSelector("#pass"));
+                    password2.SendKeys("sifra2");
+                    DoWait(1);
+                    this.Driver.Close();
+                }
+            }
+            this.Driver.SwitchTo().Window(mainWindow);
+        }
+
+
         [SetUp]
         public void SetUpTests()
         {
             this.Driver = new FirefoxDriver();
+            //this.Driver = new ChromeDriver();
             this.Driver.Manage().Cookies.DeleteAllCookies();
             this.Driver.Manage().Window.Maximize();
         }
